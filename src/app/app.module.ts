8b4afbed8715a52ecc83691b3a76cb5cca3cd001
Breno Bastos <breno17contato@hotmail.com>
2024-03-03 19:51:47 -0300

    Adicionando tela inicial e rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,14 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
-
+import { ItensHomeComponent } from './components/itens-home/itens-home.component';
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     HomeComponent,
-    HeaderComponent
+    HeaderComponent,
+    ItensHomeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/itens-home/itens-home.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-itens-home',
+  templateUrl: './itens-home.component.html',
+  styleUrl: './itens-home.component.scss'
+})
+export class ItensHomeComponent {
+
+}
